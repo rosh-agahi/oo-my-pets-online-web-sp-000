6c165b063fe7987530cb66b92ae860648b14c4bb
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-28 04:43:59 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,8 +1,19 @@
 class Owner
   attr_reader :name, :species
+  @@all = []
   
   def initialize(name)
     @name = name 
     @species = "human"
+    @@all << self 
   end
+  
+  def all 
+    @@all 
+  end
+  def count
+    self.all.count
+  end
+  def reset_all
+    
 end
